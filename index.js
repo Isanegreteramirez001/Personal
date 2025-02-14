0fc7faae4713d78c3f006b3eb1b832a07909f90b
Isanegreteramirez001 <isaiah.negrete_ramirez24@student.cart.org>
2025-02-14 15:51:13 -0800

    1

---
+++ index.js
@@ -0,0 +1,18 @@
+let spaceship = {
+    'Fuel Type': 'diesel',
+    color: 'silver'
+  };
+
+  let fasterShip = {
+    'Fuel Type': 'Quatum fuel',
+    color: 'silver'
+  };
+  'hello'.length;
+
+let Aspaceship = {
+  homePlanet: 'Earth',
+  color: 'silver'
+};
+Aspaceship.homePlanet; // Returns 'Earth',
+Aspaceship.color;
+
